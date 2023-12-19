3746e4978dbcce767362a2d1a67a0430bed7469b
VitBuk <vitbuk@gmail.com>
2023-12-19 15:40:19 +0200

    Day2 task and values

---
+++ src/day2/Day2.java
@@ -0,0 +1,12 @@
+package day2;
+
+public class Day2 {
+    private static final int RED_COUNT = 12;
+    private static final int BLUE_COUNT = 14;
+    private static final int GREEN_COUNT = 13;
+
+    public static int answer() {
+        
+    }
+
+}
