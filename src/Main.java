53f4345b3b64e72134c5fb39c4211e10f12427f1
VitBuk <vitbuk@gmail.com>
2023-12-20 18:17:16 +0200

    Day3 Part One

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 import day1.Day1;
 import day2.Day2;
+import day3.Day3;
 
 public class Main {
     public static void main(String[] args) {
-        Day2.answer();
+        Day3.answer();
     }
 }
--- src/day3/Day3.java
+++ src/day3/Day3.java
@@ -17,6 +17,8 @@ public class Day3 {
             while (sc.hasNextLine()) {
                 lines.add(sc.nextLine());
             }
+
+            System.out.println("Result: " + sumOfParts(lines));
             sc.close();
         } catch (IOException e) {
             e.printStackTrace();
@@ -26,6 +28,7 @@ public class Day3 {
     private static int sumOfParts (List<String> lines) {
         List<String> threeLines = new ArrayList<>();
         int sumOfParts = 0;
+
         for (int i=0; i < lines.size(); i++) {
             if (i > 0)
                 threeLines.add(lines.get(i-1));
@@ -40,41 +43,57 @@ public class Day3 {
                 threeLines.add(EMPTY_LINE);
 
             sumOfParts += sumOfPartsForLine(threeLines);
+            threeLines.clear();
         }
 
         return sumOfParts;
     }
 
     private static int sumOfPartsForLine(List<String> threeLines){
-        String upLine = threeLines.get(0);
         String middleLine = threeLines.get(1);
-        String downLine = threeLines.get(2);
 
         boolean inANumberSequence = false;
         boolean isAPart = false;
-        int number=0;
+        int number =0;
         int numberStart=-1;
         int numberEnd=-1;
+        int sumOfParts = 0;
 
-        List<Character> charsAround = new ArrayList<>();
+        List<Character> charsAround;
         for (int i=0; i<middleLine.length(); i++) {
             if (Character.isDigit(middleLine.charAt(i))){
                 if (!inANumberSequence) {
                     numberStart = i;
+                    System.out.println("numberStart = " + numberStart);
                 }
 
-                hasNonDigitSymbolAround(charsAround);
-            }
-            if(!inANumberSequence) {
-                if (Character.isDigit(middleLine.charAt(i))){
-                    numberStart = i;
+                inANumberSequence = true;
+                charsAround = charsAround(threeLines, i);
+                System.out.println("For char: " + middleLine.charAt(i));
+                for (char c : charsAround) {
+                    System.out.print(c);
+                    System.out.println("------");
                 }
-                if ()
+
+                if (!isAPart){
+                    isAPart = hasNonDigitSymbolAround(charsAround);
+                }
+                System.out.println("isAPart: " + isAPart);
+            } else if (inANumberSequence) {
+                numberEnd = i;
+                if (isAPart) {
+                    number = Integer.parseInt(middleLine.substring(numberStart, numberEnd));
+                    sumOfParts += number;
+                    System.out.println("Number: " + number);
+                    System.out.println("SumOfParts: " + sumOfParts);
+                    isAPart = false;
+                }
+                inANumberSequence = false;
             }
-           if (Character.isDigit(middleLine.charAt(i))){
-               numberStart = i;
-           }
+
         }
+
+        return sumOfParts;
     }
 
     private static boolean isNonDigitSymbol(Character c) {
@@ -92,7 +111,6 @@ public class Day3 {
     private static List<Character> charsAround(List<String> threeStrings, int indexOfChar){
         List<Character> charsAround = new ArrayList<>();
         int leftColumn = indexOfChar -1;
-        int sameColumn = indexOfChar;
         int rightColumn = indexOfChar +1;
 
         if (leftColumn >= 0){
@@ -100,12 +118,14 @@ public class Day3 {
             charsAround.add(threeStrings.get(1).charAt(leftColumn));
             charsAround.add(threeStrings.get(2).charAt(leftColumn));
         }
-        charsAround.add(threeStrings.get(0).charAt(sameColumn));
-        charsAround.add(threeStrings.get(2).charAt(sameColumn));
-        if (rightColumn <= threeStrings.get(0).length()) {
+        charsAround.add(threeStrings.get(0).charAt(indexOfChar));
+        charsAround.add(threeStrings.get(2).charAt(indexOfChar));
+        if (rightColumn < threeStrings.get(0).length()) {
             charsAround.add(threeStrings.get(0).charAt(rightColumn));
             charsAround.add(threeStrings.get(1).charAt(rightColumn));
             charsAround.add(threeStrings.get(2).charAt(rightColumn));
         }
+
+        return charsAround;
     }
 }
